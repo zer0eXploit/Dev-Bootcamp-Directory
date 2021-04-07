ea7d453b559b05da7548fa5525475e5e682fb4ae
Yan Waipann <yanwaipann@gmail.com>
2021-04-07 21:44:37 +0630

    Add db seeder and env example

---
+++ configs/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+// @desc    Connects to mongodb and logs hostname
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  });
+
+  console.log(`MongoDB Connected: ${conn.connection.host}`.underline.bold);
+};
+
+module.exports = connectDB;
