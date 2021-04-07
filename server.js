cad29fe0729a08ac6dd1ceec332ea7bba8234845
Yan Waipann <yanwaipann@gmail.com>
2021-04-07 14:50:36 +0630

    Add routes, middlewares and error handlers

--- server.js
+++ server.js
@@ -1,14 +1,50 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+
+const connectDB = require('./configs/db');
+const errorHandler = require('./middlewares/errorHandler');
 
 // Load ENV vars
 dotenv.config({ path: './configs/config.env' });
 
+// Connect to DB
+connectDB();
+
+// Routers
+const bootcamp = require('./routes/bootcamp');
+
 // App initialization
 const app = express();
 
+// Apply Middlewares
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'Development') {
+  app.use(morgan('dev'));
+}
+
+// Mount Routers
+app.use('/api/v1/bootcamps', bootcamp);
+
+// Error Handler Middleware
+// Call only after mounting routers
+// to catch errors occurred on routes
+app.use(errorHandler);
+
+// Port Binding
 const PORT = process.env.PORT || '3000';
 
-app.listen(PORT, () => {
-  console.log(`Running in ${process.env.NODE_ENV} mode on port ${PORT}...`);
+const server = app.listen(PORT, () => {
+  console.log(
+    `Running in ${process.env.NODE_ENV} mode on port: ${PORT}`.yellow,
+  );
+});
+
+// Handle unhandled rejections
+process.on('unhandledRejection', (error, promise) => {
+  console.log(`[Unhandled Rejection] ${error.message}`.red);
+  // Close server and exit out of the applicaton
+  server.close(() => process.exit(1));
 });
