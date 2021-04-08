7f1e0c1c3255139e5fdb39c1d3b185d184a26156
Yan Waipann <yanwaipann@gmail.com>
2021-04-08 16:05:25 +0630

    Add functionality to create and view a course

--- controllers/course.js
+++ controllers/course.js
@@ -1,4 +1,5 @@
 const Course = require('../models/Course');
+const Bootcamp = require('../models/Bootcamp');
 const asyncHandler = require('../middlewares/asyncHandler');
 const ErrorResponse = require('../utils/errorResponse');
 
@@ -25,5 +26,48 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
     success: true,
     count: courses.length,
     data: courses,
+  });
+});
+
+// @desc    Get a single course
+// @route   GET /api/v1/courses/:id
+// @access  Public
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`Course with ID ${req.params.id} is not found.`, 404),
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+// @desc    Create a course under a bootcamp
+// @route   POST /api/v1/bootcamps/:bootcampID/courses
+// @access  Private
+exports.createCourse = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampID;
+
+  const bootcamp = await Bootcamp.findById(req.params.bootcampID);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(
+        `Bootcamp with ID ${req.params.bootcampID} is not found.`,
+        404,
+      ),
+    );
+  }
+
+  course = await Course.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    data: course,
   });
 });
