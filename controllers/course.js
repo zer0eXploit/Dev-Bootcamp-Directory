ccb1ed304b8a3427dd27a49f7abb35c385eb20bc
Yan Waipann <yanwaipann@gmail.com>
2021-04-08 16:26:23 +0630

    Add method to remove a course.

--- controllers/course.js
+++ controllers/course.js
@@ -71,3 +71,26 @@ exports.createCourse = asyncHandler(async (req, res, next) => {
     data: course,
   });
 });
+
+// @desc    Update a course
+// @route   PUT /api/v1/courses/:id
+// @access  Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`Course with ID ${req.params.id} is not found.`, 404),
+    );
+  }
+
+  const updated = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: updated,
+  });
+});
