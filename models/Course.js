ec6fdf85ffac8d4239c84f76133354c97b9cfc05
Yan Waipann <yanwaipann@gmail.com>
2021-04-08 19:55:54 +0630

    Add static method to calculate and add averageCost
    A static method is attached to Course Schema and inside that,
    an aggregation is done to get the average cost of courses in
    a specific bootcamp. Then the average cost is set on pre save
    and pre remove hooks.

--- models/Course.js
+++ models/Course.js
@@ -37,4 +37,42 @@ const CourseSchema = new mongoose.Schema(
   { timestamps: { createdAt: 'firstCreated', updatedAt: 'lastUpdated' } },
 );
 
+CourseSchema.statics.setAverageCost = async function (bootcampID) {
+  // Calculate the average cost
+  const avgCost = await this.aggregate([
+    { $match: { bootcamp: bootcampID } },
+    {
+      $group: {
+        _id: '$bootcamp',
+        avgCost: {
+          $avg: '$tuition',
+        },
+      },
+    },
+  ]);
+
+  // Insert into db
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampID, {
+      averageCost: Math.ceil(avgCost[0]['avgCost'] / 10) * 10,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// Recalculate the average on each addition or removal of a courses
+CourseSchema.pre('save', function (next) {
+  // this.constructor is the Course Model itself
+  // because 'this' is an instance of the Course model
+  this.constructor.setAverageCost(this.bootcamp);
+  next();
+});
+
+CourseSchema.pre('remove', function (next) {
+  // this.constructor is the Course Model itself
+  this.constructor.setAverageCost(this.bootcamp);
+  next();
+});
+
 module.exports = mongoose.model('Course', CourseSchema);
