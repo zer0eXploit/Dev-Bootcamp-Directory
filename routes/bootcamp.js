819cebadbc9bd85b5941a8d959be641fba85cd0c
Yan Waipann <yanwaipann@gmail.com>
2021-04-08 13:23:38 +0630

    Add courses route
    Courses route is imported and used to get a lists of courses in
    a specific bootcamp.

--- routes/bootcamp.js
+++ routes/bootcamp.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+// Routers
+const courseRouter = require('./course');
+
 // Controllers
 const {
   getBootcamps,
@@ -18,6 +21,14 @@ router
   .get(getBootcamp)
   .put(updateBootcamp)
   .delete(deleteBootcamp);
+
+// Get a list of courses inside a bootcamp
+// Rather than importing the course controller here,
+// and registering a controller,
+// the route handler for courses is used.
+// Since :bootcampID is used here, merge params must be
+// passed to course router as an option.
+router.use('/:bootcampID/courses', courseRouter);
 
 router.route('/radius/:zipCode/:distance').get(getBootcampsWithinRadius);
 
