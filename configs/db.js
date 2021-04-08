eee537bed434873235b5f47bf38c5e6288b3b494
Yan Waipann <yanwaipann@gmail.com>
2021-04-08 19:45:42 +0630

    Set useFindAndModify option to false

--- configs/db.js
+++ configs/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
   const conn = await mongoose.connect(process.env.DB_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: true,
+    useFindAndModify: false,
     useUnifiedTopology: true,
   });
 
