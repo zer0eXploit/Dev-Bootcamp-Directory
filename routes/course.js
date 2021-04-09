8e64a672081372cdd2df324d2e73bbe53e0753bc
Yan Waipann <yanwaipann@gmail.com>
2021-04-09 20:31:01 +0630

    Add advancedResults middleware to /courses
    The middleware is added for filtering, sorting, etc.

--- routes/course.js
+++ routes/course.js
@@ -1,4 +1,6 @@
 const express = require('express');
+
+// Controllers
 const {
   getCourses,
   getCourse,
@@ -7,11 +9,25 @@ const {
   deleteCourse,
 } = require('../controllers/course');
 
+// Models
+const Course = require('../models/Course');
+
+// Middlewares
+const advancedResults = require('../middlewares/advancedResults');
+
 const router = express.Router({ mergeParams: true });
 
-// Requests to /bootcamps/:bootcampID/couses also hit here
+// Requests to /bootcamps/:bootcampID/courses also hit here
 // because of the forwarding at ./routes/bootcamp.js
-router.route('/').get(getCourses).post(createCourse);
+const toPopulate = {
+  path: 'bootcamp',
+  select: 'name description',
+};
+
+router
+  .route('/')
+  .get(advancedResults(Course, toPopulate), getCourses)
+  .post(createCourse);
 
 router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 
