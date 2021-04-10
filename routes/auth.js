67ba4adbb9f76e0adb7fdb1a9988722e39fb4e46
Yan Waipann <yanwaipann@gmail.com>
2021-04-10 21:39:19 +0630

    Add /me

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,15 @@
 const express = require('express');
 
 // Controlles
-const { authRegister, authLogin } = require('../controllers/auth');
+const { authRegister, authLogin, authGetMe } = require('../controllers/auth');
+
+// Middlewares
+const { protect } = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.route('/register').post(authRegister);
 router.route('/login').post(authLogin);
+router.route('/me').get(protect, authGetMe);
 
 module.exports = router;
