4847ddd4e8cd8dd1bd4f3e816f7e1e1809817e50
Yan Waipann <yanwaipann@gmail.com>
2021-04-10 20:14:14 +0630

    Add /auth/login handler

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,7 +10,6 @@ const asyncHandler = require('../middlewares/asyncHandler');
 // @access  Public
 exports.authRegister = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
-  //   await User.deleteMany();
 
   const user = await User.create({
     name,
@@ -24,5 +23,29 @@ exports.authRegister = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     success: true,
     token,
+  });
+});
+
+// @desc    Logs a user in
+// @route   POST /api/v1/auth/login
+// @access  Public
+exports.authLogin = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    return next(new ErrorResponse(`Please enter username and password.`, 400));
+
+  const user = await User.findOne({
+    email,
+  }).select('+password');
+
+  if (!user) return next(new ErrorResponse(`Bad credentials.`, 401));
+
+  const pwCorrect = await user.isPasswordCorrect(password);
+
+  if (!pwCorrect) return next(new ErrorResponse(`Bad credentials.`, 401));
+
+  res.status(200).json({
+    token: user.getSignedToken(),
   });
 });
