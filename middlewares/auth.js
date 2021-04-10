6fa3e1b4c7760ef30a8013c33d62db0e0c721691
Yan Waipann <yanwaipann@gmail.com>
2021-04-10 21:56:06 +0630

    Add role authorizer

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -43,3 +43,18 @@ exports.protect = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse(`${UNAUTHORIZED}`, 401));
   }
 });
+
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorResponse(
+          `Certain permission is not met to access this resourece.`,
+          403,
+        ),
+      );
+    }
+
+    next();
+  };
+};
