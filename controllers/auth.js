6ab09c1efd8af7e53e9a2ec76c733014ba00f3b0
Yan Waipann <yanwaipann@gmail.com>
2021-04-14 17:27:07 +0630

    Add forgot-password controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,6 +5,9 @@ const User = require('../models/User');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middlewares/asyncHandler');
 
+// Utils
+const sendEmail = require('../utils/email');
+
 // @desc    Register a user
 // @route   POST /api/v1/auth/register
 // @access  Public
@@ -41,6 +44,49 @@ exports.authLogin = asyncHandler(async (req, res, next) => {
   if (!pwCorrect) return next(new ErrorResponse(`Bad credentials.`, 401));
 
   sendTokenResponse(user, 200, res);
+});
+
+// @desc    Reset user account password.
+// @route   GET /api/v1/auth/forgot-password
+// @access  Public
+exports.authForgotPassword = asyncHandler(async (req, res, next) => {
+  const { email } = req.body;
+
+  if (!email) return next(new ErrorResponse(`Email address is required.`, 400));
+
+  const user = await User.findOne({ email });
+
+  if (!user)
+    return next(
+      new ErrorResponse(
+        `${email} is not associated with an existing user.`,
+        404,
+      ),
+    );
+
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  const url = `/api/v1/auth/reset-password/${resetToken}`;
+  const fullResetPasswordUrl = `${req.protocol}://${req.get('host')}${url}`;
+  const subject = 'Password Reset Email';
+  const text = `Please make a PUT request to the following url.\n${fullResetPasswordUrl}`;
+
+  try {
+    await sendEmail({ email: user.email, subject, text });
+  } catch (e) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorResponse(`Error sending password reset email.`, 500));
+  }
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
 });
 
 // @desc    Get logged in user information.
