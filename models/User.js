9469be1133002901c2977a97acdc93b895399b75
Yan Waipann <yanwaipann@gmail.com>
2021-04-14 16:02:13 +0630

    Pass a hook execution if password is not modified

--- models/User.js
+++ models/User.js
@@ -64,6 +64,8 @@ UserSchema.methods.getResetPasswordToken = function () {
 };
 
 UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) next();
+
   const salt = await bcrypt.genSalt(10);
 
   this.password = await bcrypt.hash(this.password, salt);
