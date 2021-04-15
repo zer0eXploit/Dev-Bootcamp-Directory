33f077b20aab463cb2298b49f706185b2f889cab
Yan Waipann <yanwaipann@gmail.com>
2021-04-15 16:44:56 +0630

    Add GET reviews route

---
+++ routes/review.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+// Controllers
+const { getReviews } = require('../controllers/review');
+
+// Models
+const Review = require('../models/Review');
+
+// Middlewares
+const advancedResults = require('../middlewares/advancedResults');
+const { protect } = require('../middlewares/auth');
+
+const router = express.Router({ mergeParams: true });
+
+// Requests to /bootcamps/:bootcampID/reviews also hit here
+// because of the forwarding at ./routes/bootcamp.js
+const toPopulate = {
+  path: 'bootcamp',
+  select: 'name description',
+};
+
+router.route('/').get(advancedResults(Review, toPopulate), getReviews);
+
+module.exports = router;
