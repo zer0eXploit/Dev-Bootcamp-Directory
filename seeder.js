4233a12469e09b28bc5ea87d6e651478560ea25f
Yan Waipann <yanwaipann@gmail.com>
2021-04-15 19:37:49 +0630

    Add review seeder and destroyer

--- seeder.js
+++ seeder.js
@@ -11,6 +11,7 @@ dotenv.config({ path: './configs/config.env' });
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
 const User = require('./models/User');
+const Review = require('./models/Review');
 
 // Connect to MongoDB
 mongoose
@@ -31,10 +32,9 @@ mongoose
 const JSONPath = `${__dirname}/data`;
 
 const bootcamps = JSON.parse(fs.readFileSync(`${JSONPath}/bootcamps.json`));
-
 const courses = JSON.parse(fs.readFileSync(`${JSONPath}/courses.json`));
-
 const users = JSON.parse(fs.readFileSync(`${JSONPath}/users.json`));
+const reviews = JSON.parse(fs.readFileSync(`${JSONPath}/reviews.json`));
 
 // Seed into database
 const seedToDB = async () => {
@@ -42,6 +42,7 @@ const seedToDB = async () => {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
     await User.create(users);
+    await Review.create(reviews);
     console.log(`Data seeded.`.green.inverse);
     process.exit();
   } catch (e) {
@@ -55,6 +56,7 @@ const destroyDB = async () => {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
     await User.deleteMany();
+    await Review.deleteMany();
     console.log(`All data deleted.`.green.inverse);
     process.exit();
   } catch (e) {
