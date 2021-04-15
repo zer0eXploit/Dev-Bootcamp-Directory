0009eb1a6e713a6546e8fab70b70261074ca0878
Yan Waipann <yanwaipann@gmail.com>
2021-04-15 19:36:53 +0630

    Add handler for GET a single review

--- controllers/review.js
+++ controllers/review.js
@@ -14,6 +14,9 @@ exports.getReviews = asyncHandler(async (req, res, next) => {
   if (req.params.bootcampID) {
     const reviews = await Review.find({
       bootcamp: req.params.bootcampID,
+    }).populate({
+      path: 'user',
+      select: 'name',
     });
 
     res.status(200).json({
@@ -24,4 +27,29 @@ exports.getReviews = asyncHandler(async (req, res, next) => {
   } else {
     res.status(200).json(res.advancedResults);
   }
+});
+
+// @desc    Get a single review
+// @route   GET /api/v1/reviews/:id
+// @access  Public
+exports.getReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id)
+    .populate({
+      path: 'user',
+      select: 'name',
+    })
+    .populate({
+      path: 'bootcamp',
+      select: 'name',
+    });
+
+  if (!review)
+    return next(
+      new ErrorResponse(`Review with ID ${req.params.id} is not found.`, 404),
+    );
+
+  res.status(200).json({
+    success: true,
+    data: review,
+  });
 });
