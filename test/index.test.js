d1b2781d2ccea1f4ab037feeafeedca91f6cec36
Yan Waipann <yanwaipann@gmail.com>
2021-04-18 16:20:06 +0630

    Add index route testing

---
+++ test/index.test.js
@@ -0,0 +1,27 @@
+const app = require('../app');
+
+const expect = require('chai').expect;
+const request = require('supertest')(app);
+
+describe('Index Route Tests', function () {
+  it('Should respond to a request.', async function () {
+    await request.get('/');
+  });
+
+  it('Should not respond with a 404 status.', async function () {
+    const statusCode = await (await request.get('/')).status;
+    expect(statusCode).to.not.equal(404);
+  });
+
+  it('Should respond with a status of 200.', async function () {
+    await request.get('/').expect(200);
+  });
+
+  it('Should respond with exact json value.', async function () {
+    const responseObject = {
+      API_DOCS: 'https://documenter.getpostman.com/view/8103362/TzJrCeZN',
+    };
+    const response = await request.get('/');
+    expect(response.body).to.be.deep.equal(responseObject);
+  });
+});
